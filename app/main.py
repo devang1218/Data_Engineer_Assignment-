e4113ee1f14be3d6b66ad3fa8d4db066db29e115
Devang <sharmadevang12@gmail.com>
2024-04-19 23:51:15 +0530

    updated code with the integration of provided database

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ import pandas as pd
 import sqlite3
 
 # first method using sql query 
-path_to_db = "D:\projects\\app\Data Engineer - Assignment Database@eastVantage.db"
+# path to the database file
+path_to_db = "D:\projects\database\Data Engineer - Assignment Database@eastVantage.db"
+
 db = database(path = path_to_db)
 
 result_sql = db.get_targeted_customers()
@@ -14,17 +16,17 @@ result_sql.to_csv(path_or_buf='../output/sql_query_output.csv',sep=';', index=Fa
 # Second method using Pandas
 cnx = sqlite3.connect(path_to_db)
 
-df_item = pd.read_sql_query("SELECT * FROM Items", cnx)
-df_customer = pd.read_sql_query("SELECT * FROM Customers", cnx)
+df_items = pd.read_sql_query("SELECT * FROM Items", cnx)
+df_customers = pd.read_sql_query("SELECT * FROM Customers", cnx)
 df_orders = pd.read_sql_query("SELECT * FROM Orders", cnx)
 df_sales = pd.read_sql_query("SELECT * FROM Sales", cnx)
 
-df = (df_customer.merge(
+df = (df_customers.merge(
                     df_sales, on= 'customer_id'
                 ).merge(
                     df_orders, on= 'sales_id'
                 ).merge(
-                    df_item, on= 'item_id'
+                    df_items, on= 'item_id'
                 )
                 .query('age >= 18 and age <= 35')
                 .groupby(['customer_id', 'age', 'item_id','item_name'])
@@ -39,6 +41,8 @@ result_df = df[['customer_id', 'age', 'item_name', 'Quantity']].rename(columns={
     'item_name': 'Item',
     'quantity': 'Quantity'
 })
+
+result_df['Quantity'] = result_df['Quantity'].astype(int)
 
 # storing the final result using pandas in csv (;) separated files in output folder 
 result_df.to_csv(path_or_buf='../output/pandas_query_output.csv',sep=';', index=False)
